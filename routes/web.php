d7986186770843102ef32787762615e1b94c3ada
matheusaltair <matheusaltair13@gmail.com>
2021-12-11 14:38:10 -0300

    atualização routes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RevendaController;
 
+Route::get('/', function () {
+    return redirect('/revenda');
+});
 Route::resource('posts', PostController::class);
 Route::resource('revenda', RevendaController::class);
